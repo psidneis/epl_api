7bb22ac5ad043f1ae36dc2967d41c59be834bee8
Sidnei Peeira <p.sidneis@gmail.com>
2017-03-11 11:49:01 -0300

    fix routes and available actions for job resource

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,38 @@
+module Api::V1
+  class JobsController < ApiController
+    before_action :set_job, only: [:activate]
+
+    def index
+      @jobs = Job.all
+
+      render json: @jobs
+    end
+
+    def create
+      @job = Job.new(job_params)
+
+      if @job.save
+        render json: @job, status: :created, location: @job
+      else
+        render json: @job.errors, status: :unprocessable_entity
+      end
+    end
+
+    def activate
+      if @job.activate(job_params)
+        render json: @job
+      else
+        render json: @job.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+      def set_job
+        @job = Job.find(params[:id])
+      end
+
+      def job_params
+        params.require(:job).permit(:partner_id, :title, :category_id, :expires_at)
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :jobs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :jobs, only: [:index, :create] do
+        	patch :activate
+        end
+      end
+    end
+  end
+
 end
